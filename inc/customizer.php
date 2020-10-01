b846eb63a8db43e8d3faf542619b0abc3fd1770d
Florent <florent.chatelet@edu.univ-fcomte.fr>
2020-10-01 08:30:08 +0200

    Initialisation des fichiers underscore

---
+++ inc/customizer.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * MesVoitures Theme Customizer
+ *
+ * @package MesVoitures
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function mesvoitures_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial(
+			'blogname',
+			array(
+				'selector'        => '.site-title a',
+				'render_callback' => 'mesvoitures_customize_partial_blogname',
+			)
+		);
+		$wp_customize->selective_refresh->add_partial(
+			'blogdescription',
+			array(
+				'selector'        => '.site-description',
+				'render_callback' => 'mesvoitures_customize_partial_blogdescription',
+			)
+		);
+	}
+}
+add_action( 'customize_register', 'mesvoitures_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function mesvoitures_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function mesvoitures_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function mesvoitures_customize_preview_js() {
+	wp_enqueue_script( 'mesvoitures-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
+}
+add_action( 'customize_preview_init', 'mesvoitures_customize_preview_js' );
